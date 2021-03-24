8b6448347722b2bfbd6a596eb3672efc213c46ba
murilosilverio78 <murilo.silverio@hotmail.com>
2021-03-24 19:59:25 -0300

    fim da aula

---
+++ aula12/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/'
+});
+
+const listaUsuarios = [
+    'Ivens',
+    'Jailson',
+    'Maria',
+    'Natalia',
+    'Ariel',
+    'Nykolle'
+]
+
+app.use(express.static(path.join(__dirname, '/src/assets/')));
+
+// Query Params
+// Ex: localhost:3000/usuarios?nome=Ivens
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno);
+});
+
+// Route Params 
+// Ex: ecommer.com.br/produtos/4/detalhes
+app.get('/hello/:usuario', (req, res) => {    
+    const { usuario } = req.params;
+    return res.send(`Hello world ${usuario}`);
+});
+
+app.get('/pokemons', async (req, res) => {
+    const responseAxios = await api.get('pokemon?limit=100&offset=200');
+    const { data } = responseAxios;
+    const retorno = {
+        ...data,
+        results: data.results.map(item => {
+            item.greetings = 'Olá';            
+            return item;
+        })
+    }
+    return res.json(retorno);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/index.html'));
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/enviar-mensagem', (req,res) => {
+    const {name, email, comentario} = (req.body)
+    if (name &&  email && comentario) {
+        
+    }
+    return res.send(name)
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
